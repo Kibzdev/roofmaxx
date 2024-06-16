14e46da97b203656fc1b0152d6c67bf0fb86e9e8
Fredrick <dev@kvuliagency.com>
2024-06-16 22:45:47 +0300

    navbar bottom re alighn

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,10 +29,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
       </Head>
       <body className={`inter.className} overflow-x-hidden`}>
         <Header/>
-        
+        <BottomMenu/>
         {children}
         <Footer/>
-        <BottomMenu/>
+       
       </body>
     </html>
   );
