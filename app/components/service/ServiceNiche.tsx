910571a80d54462b3450036720637679322f629f
Fredrick <dev@kvuliagency.com>
2024-06-16 21:40:04 +0300

    service card type update

--- app/components/service/ServiceNiche.tsx
+++ app/components/service/ServiceNiche.tsx
@@ -11,17 +11,19 @@ interface NicheCardProps {
 const NicheCard: React.FC<NicheCardProps> = ({ niches }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-4 w-full h-full">
-      {niches.map((niche, index) => (
-        <Link key={index} href={`/niche/${niche.slug}`}>
+      {niches.map((niche) => (
+        <Link key={niche.slug.current} href={`/niche/${niche.slug.current}`}>
           <Card className="border-b-4 border-b-[#ef4444] bg-[#075985] shadow-b-md shadow-[#ef4444] lg:w-80 md:w-80">
             <CardHeader className="relative w-full h-64 sm:h-80 flex justify-center items-center bg-gray-200 overflow-hidden rounded-sm shadow-lg shadow-black">
-              <Image
-                src={niche.niche_banner.asset.url}
-                alt={niche.niche_name}
-                layout="fill"
-                sizes="100vw"
-                className="rounded-sm object-cover grayscale hover:grayscale-0 transition duration-300 ease-in-out"
-              />
+              {niche.niche_banner && niche.niche_banner.asset && (
+                <Image
+                  src={niche.niche_banner.asset.url}
+                  alt={niche.niche_name}
+                  layout="fill"
+                  sizes="100vw"
+                  className="rounded-sm object-cover grayscale hover:grayscale-0 transition duration-300 ease-in-out"
+                />
+              )}
             </CardHeader>
             <CardContent className="text-center p-4">
               <h2 className="text-white text-lg font-bold mt-2 uppercase">{niche.niche_name}</h2>
@@ -31,6 +33,6 @@ const NicheCard: React.FC<NicheCardProps> = ({ niches }) => {
       ))}
     </div>
   );
-}
+};
 
 export default NicheCard;
