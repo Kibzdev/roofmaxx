ee6d7a9e7882a27e8948b1f2ee1c0c8df9a2cbb3
Fredrick <dev@kvuliagency.com>
2024-07-03 08:39:27 +0300

    CTO About

--- app/components/about/GetStartedCTO.tsx
+++ app/components/about/GetStartedCTO.tsx
@@ -5,30 +5,28 @@ import Container from '../Container';
 const GetStartedCTO: React.FC = () => {
   return (
     <Container>
-      <div className={clsx("flex flex-col md:flex-row items-center justify-between bg-gray-100 p-4", "md:p-8")}>
-      <div>
-        <h2 className="text-2xl font-semibold text-gray-800">Ready to Get Started</h2>
-        <p className="text-gray-600 mt-2">
-          Nulla Lorem mollit cupidatat irure. nulla duis ullamco cillum dolor. 
-          Voluptate exercitation incididunt aliquip.
-        </p>
+      <div className={clsx("flex flex-col md:flex-row items-center justify-between bg-red-500 text-white p-4", "md:p-8")}>
+        <div>
+          <h2 className="text-2xl font-semibold">Ready to Get Started</h2>
+          <p className="mt-2 text-sm md:text-lg font-medium tracking-wider">
+            By entering your email, you’ll gain access to the latest innovations, expert advice,
+          </p>
+        </div>
+        <form className={clsx("flex flex-col md:flex-row items-center mt-4 md:mt-0", "space-y-4 md:space-y-0 md:space-x-4")}>
+          <input
+            type="email"
+            placeholder="Enter your email ..."
+            className="form-input px-4 py-2 border border-gray-300 rounded-md text-sky-800 focus:outline-none focus:border-sky-800 hover:border-sky-700 focus:ring focus:ring-sky-900"
+          />
+          <button
+            type="submit"
+            className="bg-sky-900 hover:bg-sky-800 text-white font-bold py-2 px-4 rounded-md transition-colors"
+          >
+            SUBSCRIBE
+          </button>
+        </form>
       </div>
-      <form className={clsx("flex flex-col md:flex-row items-center mt-4 md:mt-0", "space-y-4 md:space-y-0 md:space-x-4")}>
-        <input
-          type="email"
-          placeholder="Enter your email ..."
-          className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md transition-colors"
-        >
-          SUBSCRIBE
-        </button>
-      </form>
-    </div>
     </Container>
-    
   );
 };
 
