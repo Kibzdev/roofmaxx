314fb6efc3e1548c7a1d968666d9b0befd782570
Fredrick <dev@kvuliagency.com>
2024-07-03 07:16:00 +0300

    about banner top

--- app/components/about/AboutBanner.tsx
+++ app/components/about/AboutBanner.tsx
@@ -6,7 +6,7 @@ const ServiceBanner: React.FC = () => {
   // TypeScript definitions aren't required here as no props or complex states are used
 
   return (
-    <div className="relative w-full max-h-screen mt-0">
+    <div className="relative w-full max-h-screen mt-20">
       <Image 
         src={aboutbg}
         alt="Service Image" // Modified alt text for better clarity
