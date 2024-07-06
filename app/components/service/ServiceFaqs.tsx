400e9e9951fffc537093646ec323bc3be68dc024
Fredrick <dev@kvuliagency.com>
2024-07-07 00:16:40 +0300

    service Faq text-sizing

--- app/components/service/ServiceFaqs.tsx
+++ app/components/service/ServiceFaqs.tsx
@@ -25,11 +25,11 @@ const ServiceFaqs: React.FC<ServiceFaqsProps> = ({ faqs = [] }) => {
       <Accordion type="single" collapsible className="w-full">
         {faqs.map((faq: FaqItem) => (
           <AccordionItem key={faq._key || uuidv4()} value={faq._key || uuidv4()}>
-            <AccordionTrigger className=" text-lg  md:text-xl font-semibold tracking-wider text-center text-sky-800">
+            <AccordionTrigger className=" text-lg  md:text-xl font-medium tracking-wide text-center text-sky-800">
               {faq.question}
             </AccordionTrigger>
             <AccordionContent className="transition-height duration-500 ease-in-out">
-              <p className="text-lg text-gray-500 tracking-wider leading-5 font-medium">{faq.answer}</p>
+              <p className="text-lg text-gray-500 tracking-normal leading-6 font-normal">{faq.answer}</p>
             </AccordionContent>
           </AccordionItem>
         ))}
