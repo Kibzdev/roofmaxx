69b17f0100b3857260ddbbb1eae21ebfaef61c13
Fredrick <dev@kvuliagency.com>
2024-07-08 02:06:27 +0300

    bottom menu reduce

--- app/components/BottomMenu.tsx
+++ app/components/BottomMenu.tsx
@@ -5,16 +5,16 @@ const BottomMenu: React.FC = () => {
   return (
     <div className="fixed bottom-0 w-full bg-white border-t border-gray-200 shadow-md md:hidden z-50">
       <div className="flex justify-around items-center py-2">
-        <Link href="/" className="flex flex-col items-center text-center text-sky-800">
-          <HomeIcon className="w-8 h-8" />
+        <Link href="/" className="flex flex-col items-center text-center text-red-500 hover:text-sky-800  hover:translate-x-1">
+          <HomeIcon className="w-6 h-6" />
           <span className="text-sm">Home</span>
         </Link>
-        <a href="https://wa.me/yourwhatsapplink" className="flex flex-col items-center text-center text-sky-800">
-          <ChatIcon className="w-8 h-8" />
+        <a href="https://wa.me/yourwhatsapplink" className="flex flex-col items-center text-center text-red-500 hover:text-sky-800  hover:translate-x-1">
+          <ChatIcon className="w-6 h-6" />
           <span className="text-sm">WhatsApp</span>
         </a>
-        <a href="mailto:sales@info.roofmax.co.ke" className="flex flex-col items-center text-center text-sky-800">
-          <MailIcon className="w-8 h-8" />
+        <a href="mailto:sales@info.roofmax.co.ke" className="flex flex-col items-center text-center text-red-500 hover:text-sky-800  hover:translate-x-1">
+          <MailIcon className="w-6 h-6" />
           <span className="text-sm">E-mail</span>
         </a>
       </div>
