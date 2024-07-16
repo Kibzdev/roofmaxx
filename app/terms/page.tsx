6ea889d544c2c4fa822f732450698968ac65ef16
Fredrick <dev@kvuliagency.com>
2024-07-16 23:34:57 +0300

    terms & conditions

---
+++ app/terms/page.tsx
@@ -0,0 +1,213 @@
+import React from 'react'
+
+const Terms = () => {
+  return (
+    <div>
+       <div className='mx-auto mt-20 md:mt-40 bg-white shadow-md md:mx-40 md:max-w-xl8 px-8 mb-36 py-20'>
+       <div className='mt-4 mb-6'>
+            <h1 className='text-lg  md:text-4xl font-semibold  text-sky-800 text-center'>Terms & Conditions</h1>
+          </div>
+          <h3 className='font-semibold text-gray-900 text-lg'>Introduction</h3>
+          <ul className="list-disc pl-6 py-2">
+            <li>
+              <p>This agreement shall constitute the whole contract between Roofmax (referred to as “The Contractor” in the remainder of this document) and The Customer or entity. (referred to as “The Customer” in the remainder of this document)</p>
+            </li>
+            <li>
+              <p>It is the Customer’s responsibility to provide appropriate plans, specifications, or other design documents to Roofmax that are sufficient and conform to all applicable laws and building codes. </p>
+            </li>
+            <li>
+              <p>Roofmax shall not be held responsible for any loss, damage, or expense due to defects in plans or specifications or building code violations unless such damage results from a deviation by Roofmax from what is specified.</p>
+            </li>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Quotations</h3>
+          <ul className="list-disc pl-6 py-2">
+            <li>
+              <p>All prices given in quotations to the Customer are given on a strictly nett basis and are open only for a period of 30 days (unless otherwise stated) from the date of quotation. Provisional sums quoted in any quotation are not binding on the Contractor until confirmed and are subject to adjustment once sums have been finalised.</p>
+            </li>
+            <li>
+              <p>Emergency repair works and additional works carried out by the Contractor as are necessary (including the purchase of any additional materials) will be charged to the Customer in addition to any quoted price given by the Contractor and shall be at the Contractors standard hourly rate for such works.</p>
+            </li>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Measurements</h3>
+          <ul className="list-disc pl-6 py-2">
+            <li>
+              <p>In the event that measurements have been supplied by the Customer to the Contractor for the purposes of giving a quotation then the Contractor shall have the right to alter its quotation if the Contractor finds that such measurements are incorrect.</p>
+            </li>
+            <li>
+              <p>Where applicable the customer is responsible for the accuracy of any drawings, design and/or specifications submitted to the Contractor by or on behalf of the customer and the customer will indemnify the Contractor and keep it indemnified against any cost, claim, liabilities or damage caused by any errors, omissions or inaccuracies in such drawings, designs and / or specifications.</p>
+            </li>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Materials</h3>
+          <ul className="list-disc pl-6 py-2">
+            <li>
+              <p>Materials supplied by us will remain our property until paid for in full.</p>
+            </li>
+            <li>
+              <p>We reserve the right to offer substitutions when supplying the Services. The substitutions will not be inferior to those described in the Quotation.</p>
+            </li>
+            <li>
+              <p>Any samples provided to you, are samples to show substance and general character. We cannot guarantee that the products supplied will be exactly equal in colour, size, thickness, or shape. We have no responsibility if materials supplied do not exactly equate with samples previously supplied.</p>
+            </li>
+            <li>
+              <p>Salvage Materials, including scrap lead, slates, ridge tiles and general roofing materials, stripped or removed whilst providing the Services shall become our property. This will be allowed for in the Quotation.</p>
+            </li>
+            <li>
+              <p>This Agreement gives no warranty for Materials supplied by you.</p>
+            </li>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Change in Work Plan</h3>
+          <ul className="list-disc pl-6 py-2">
+            <li>
+              <p>All changes and deviations in the Work require a written change order signed by both Roofmax and Customer, and the contract price will be increased or decreased accordingly by us. </p>
+            </li>
+           
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Title and Risk</h3>
+          <ul className="list-disc pl-6 py-2">
+            <li>
+              <p>The risk in the Goods shall pass to the Customer on completion of delivery.</p>
+            </li>
+            <li>
+              <p>Title to the Goods shall not pass to the Customer until the Contractor receives payment in full for the Goods and any other goods and Services the Contractor has supplied to the Contractor in respect of which payment has become due, in which case title to the Goods shall pass at the time of payment of all such sums.</p>
+            </li>
+           
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Suspension and Termination</h3>
+          <ul className="list-disc pl-6 py-2">
+          <p className='text-gray-600'>Without limiting its other rights or remedies, the Contractor may (at its discretion) terminate the Contract or suspend the supply of Services with immediate effect by giving written notice to the Customer if:</p>
+            <li>
+              <p>the Customer fails to pay any amount due under this Contract on the due date for payment;</p>
+            </li>
+            <li>
+              <p>in the Contractor’s reasonable opinion, the Customer’s financial position deteriorates to such an extent that in the Contractor’s opinion the Customer’s capability to adequately fulfil its obligations under the Contract is placed in jeopardy.</p>
+            </li>
+            <li>
+              <p>If for any reason the Contractor is unable to complete the works, the Contractor&#39;s liability will not exceed the value of the uncompleted part of the works and the customer will remain liable to pay for the value of the completed part.
+              </p>
+            </li>
+           
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Payment:</h3>
+          <ul className="list-disc pl-6 py-2">
+            <li>
+              <p>Once quote is accepted, the quote is a fixed agreed price for work to be carried out and will not be negotiable at the completion of works. The work will be clearly stated on the quote. This is to let both parties know of the work entered into. If the scope of works is not on the quote, then it is not going to be carried out. Unfortunately, we do not work on hearsay; he said, she said. In the event of a variation to the quoted scope of works for unforeseen reasons, the customer will be notified as soon as possible.
+              </p>
+            </li>
+            <li>
+              <p>Contractor reserves the right to require a deposit in excess of 30%.
+              </p>
+            </li>
+            <li>
+              <p>The Customer will be notified as soon as practicable of any such unforeseen work and The Contractor will pass any agreed costs, due to such factors, on to the Customer.
+              </p>
+            </li>
+            <li>
+              <p>The balance of the Quotation/Contract price is strictly to be paid on the day of the installation/repair/restoration work is completed. Payments can be by Cash or Bank Transfer.
+              </p>
+            </li>
+            <li>
+              <p>All power, water and security charges incurred in relation to the work will be borne by the customer.
+              </p>
+            </li>
+            <li>
+              <p>Any materials on site in conjunction with each contract remain the property of Roofmax, until payment is made in full.
+              </p>
+            </li>
+            <li>
+              <p>Any sums that remain outstanding after 10 days following completion of the works, will be subject to a late payment charge of 10% above the CBK base rate.
+              </p>
+            </li>
+           
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Utilities: </h3>
+          <ul className="list-disc pl-6 py-2">
+          
+            <li>
+              <p>The Customer shall provide The Contractor with adequate access to electricity and other utilities as needed, the work site, and the work area adjacent to the structure. </p>
+            </li>
+            <li>
+              <p>In situations where, free power is not available or practical, a generator will be used and charged to the Customer accordingly at cost plus 20%.</p>
+            </li>
+          </ul>
+
+          <h3 className='font-semibold text-gray-900 text-lg'>Health & Safety </h3>
+          <ul className="list-disc pl-6 py-2">
+          
+            <li>
+              <p>It is the Customer responsibility to ensure that the site where the Contractor and its employees will be working is secure, clear and safe. Whilst on site the Customer must comply with any instructions as regards safety which are given by the Contractor. The Contractor reserves the right to withdraw its employees if the site or facilities on the site do not accord with current health and safety regulations or if the weather conditions are adverse. </p>
+            </li>
+            <li>
+              <p>The Customer is expected to; provide clear access for operatives and materials; limit traffic of pedestrians around the Works Area; prohibit the movement of young children and animals around the Works Area. The Customer should treat the Works Area as strictly off-limits for the duration of The Works. Customers are not permitted to climb on ladders or scaffolding.</p>
+            </li>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Schedule: </h3>
+          <ul className="list-disc pl-6 py-2">
+          
+            <li>
+              <p>Any dates given by the Contractor to the Customer for the commencement or completion of work are given for guidance only and are not binding on the Contractor. </p>
+            </li>
+            <li>
+              <p>Where possible, the works will commence on the date agreed and will be completed in most cases, subject to weather, within a reasonable timescale. Where detailed dates for commencement are presented, the contractor reserves the right to re-schedule works where delays have been incurred through adverse weather</p>
+            </li>
+            <li>
+              <p>The Contractor shall not be liable for any delay in the completion of the work that arises from circumstances beyond its reasonable control. Time taken up by such delays will not count for the purposes of this clause.</p>
+            </li>
+            <li>
+              <p>Roofmax Specialist does not accept responsibility and is not liable for leaks and damage or any associated costs resulting from storms, extreme weather, or acts of God.</p>
+            </li>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Cancellation: </h3>
+          <ul className="list-disc pl-6 py-2">
+          
+            <li>
+              <p>If the customer chooses to cancel work, they will forfeit any deposit money paid, subject to the expiry of any cooling-off period.
+              </p>
+            </li>
+           
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Complaints</h3>
+          <ul className="list-disc pl-6 py-2">
+              <p>We hope that you are happy with the service we provide. However, if at any stage, you have concerns or wish to make a complaint please tell the Site Supervisor handling your project straight away about the nature of your concern. If you do not feel comfortable speaking with the Site Supervisor, then you can contact our Business Manager directly on info@roofmax.co.ke . If the Site Supervisor cannot promptly resolve your concerns, then it will be dealt with as a formal complaint under our Complaints Policy.
+              </p>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Warranty Disclaimer: </h3>
+          <ul className="list-disc pl-6 py-2">
+              <p>We agree to convey to the Customer any manufacturer’s or supplier’s warranties on individual materials, products or systems supplied by us under this Contract. 
+              </p>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'>Liability and Indemnity
+          </h3>
+          <ul className="list-disc pl-6 py-2">
+          <li>
+              <p>Neither party shall be liable to the other for lost profits of business, indirect, consequential or punitive damages, whether based in contract or tort (including negligence, strict liability or otherwise), and whether advised of the possibility of such damages.
+              </p>
+            </li>
+            <li>
+              <p>We will not be liable for any special, indirect, or consequential loss or damage of any kind (whether foreseeable or known or not) including loss of profit, revenue, income, business, opportunity, goodwill or similar economic loss or damage.</p>
+            </li>
+            <li>
+              <p>We shall not be liable to you for any loss or damage arising because of ‘force majeure’ (that is, if we are unable to perform any of our services because of a cause beyond our reasonable control).</p>
+            </li>
+            <li>
+              <p>We will not be liable for any services or product provided by any third party even if instructed by us on your behalf or utilised by us in the provision of our services to you.</p>
+            </li>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'> SEVERANCE </h3>
+          <ul className="list-disc pl-6 py-2">
+              <p>If any provision or provisions of our Agreement including These Terms is/are found to be unlawful, void or otherwise unenforceable then it is agreed that the remainder of our Agreement including These Terms shall remain valid and enforceable 
+              </p>
+          </ul>
+          <h3 className='font-semibold text-gray-900 text-lg'> AGREEMENT </h3>
+          <ul className="list-disc pl-6 py-6">
+              <p>Unless otherwise agreed, these terms of services apply.</p>
+              <p>Your acceptance of our Quotation will be deemed to constitute your acceptance of these terms and conditions of services.
+              </p>
+              <p>Unless otherwise agreed in writing to the contrary, the advice provided and the work carried out by us in relation to any matter is intended to be relied on only by you and no other person, and we accept no liability to third parties.
+              </p>
+          </ul>
+   </div>
+    </div>
+  )
+}
+
+export default Terms
