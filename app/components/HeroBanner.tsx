cdff5b60ee011dffbab00b53eb787b87c1e2a062
Fredrick <dev@kvuliagency.com>
2024-07-17 14:15:31 +0300

    mobile all heading size reduce

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -43,8 +43,8 @@ const HeroBanner: React.FC = () => {
     />
     <FadeIn>
     <div className='absolute bottom-4 right-12 p-4 bg-black/20'>
-      <h1 className='text-white text-6xl md:text-2xl font-bold'>MAKING</h1>
-      <h1 className='text-white text-6xl md:text-2xl font-bold'>ROOFS <span className='text-red-500'>LAST</span></h1>
+      <h1 className='text-white text-4xl md:text-2xl font-bold'>MAKING</h1>
+      <h1 className='text-white text-4xl md:text-2xl font-bold'>ROOFS <span className='text-red-500'>LAST</span></h1>
     </div>
     </FadeIn>
     <div className='absolute bottom-4 right-12 p-4 bg-black/20'>
