8ccf46dce6c171721929a293550abf03c6b9eec7
Fredrick <dev@kvuliagency.com>
2024-07-17 14:12:15 +0300

    mobile banner add text

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -4,6 +4,7 @@ import { homebg, mobileBg } from '@/public/assets'; // Import your mobile backgr
 import NavSocials from './NavSocials';
 import Image from 'next/image';
 import ScheduleForm from './ScheduleForm'; // Import the new component
+import FadeIn from './FadeIn';
 
 const HeroBanner: React.FC = () => {
   const [result, setResult] = useState<Record<string, string>>({});
@@ -32,16 +33,27 @@ const HeroBanner: React.FC = () => {
       
       </div>
       <div className="absolute mt-4 inset-0 block md:hidden z-20">
-        <div className='w-full h-[340px] md:h-[640px]'>
-          <Image
-            src={homebg}
-            alt="Mobile Background Image"
-            height={340}
-            width={500}
-            className="w-full h-[340px] md:h-[640px] object-cover group-hover:scale-105 duration-500 rounded-tl-md rounded-bl-md"
-          />
-        </div>
-      </div>
+  <div className='relative w-full h-[340px] md:h-[640px]'>
+    <Image
+      src={mobileBg}
+      alt="Mobile Background Image"
+      height={340}
+      width={500}
+      className="w-full h-[340px] md:h-[640px] object-cover group-hover:scale-105 duration-500 rounded-tl-md rounded-bl-md"
+    />
+    <FadeIn>
+    <div className='absolute bottom-4 right-12 p-4 bg-black/20'>
+      <h1 className='text-white text-6xl md:text-2xl font-bold'>MAKING</h1>
+      <h1 className='text-white text-6xl md:text-2xl font-bold'>ROOFS <span className='text-red-500'>LAST</span></h1>
+    </div>
+    </FadeIn>
+    <div className='absolute bottom-4 right-12 p-4 bg-black/20'>
+      <h1 className='text-white text-6xl md:text-2xl font-bold'>MAKING</h1>
+      <h1 className='text-white text-6xl md:text-2xl font-bold'>ROOFS <span className='text-red-500'>LAST</span></h1>
+    </div>
+  </div>
+</div>
+
       <ScheduleForm className="mt-[480px] md:ml-44" /> {/* Add margin-left class to align right */}
       <div className="relative mt-0 md:mt-48 w-full md:w-2/3 flex justify-center h-full">
         {/* Optionally, additional content can go here */}
