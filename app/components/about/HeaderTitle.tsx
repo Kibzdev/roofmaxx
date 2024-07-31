796bf7ba9f3bf5329f7a68c0ad7d1eaa968e8f3c
Fredrick <dev@kvuliagency.com>
2024-07-31 15:16:06 +0300

    ml about header title

--- app/components/about/HeaderTitle.tsx
+++ app/components/about/HeaderTitle.tsx
@@ -4,12 +4,12 @@ import PageIntro from '../PageIntro';
 
 const HeaderTitle: React.FC = () => {
   return (
-    <>
+    <div className='ml-20'>
      <PageIntro eyebrow="Know Us" title="Roofmax">
         <p className='font-normal text-sm md:text-lg text-gray-600 leading-6'>We can not wait to hear from you.</p>
       </PageIntro>
       
-    </>
+    </div>
     
     
   );
--- app/components/about/OurClient.tsx
+++ app/components/about/OurClient.tsx
@@ -72,7 +72,7 @@ const OurClient: React.FC = () => {
           <div
             className={clsx(
               "mb-8 md:mb-0",
-              "md:flex md:flex-col md:justify-center md:pl-0 md:w-full"
+              "md:flex md:flex-col md:justify-center md:pl-0 md:w-full bg-red-200"
             )}
           >
             <h1 className="text-xl md:text-4xl font-bold leading-tight text-sky-800 uppercase">
