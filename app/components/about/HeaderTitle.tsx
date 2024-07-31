b623c74417795a8b1d03a9ffa5f3db3b539025ca
Fredrick <dev@kvuliagency.com>
2024-07-31 15:20:30 +0300

    ml about header min

--- app/components/about/HeaderTitle.tsx
+++ app/components/about/HeaderTitle.tsx
@@ -4,7 +4,7 @@ import PageIntro from '../PageIntro';
 
 const HeaderTitle: React.FC = () => {
   return (
-    <div className='ml-20'>
+    <div className='ml-10'>
      <PageIntro eyebrow="Know Us" title="Roofmax">
         <p className='font-normal text-sm md:text-lg text-gray-600 leading-6'>We can not wait to hear from you.</p>
       </PageIntro>
