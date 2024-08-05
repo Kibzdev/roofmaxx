87380d95ca4f55e9927c8bc1deac876880298405
Fredrick <dev@kvuliagency.com>
2024-08-05 18:39:43 +0300

    max-w remove

--- app/components/ContactFormy.tsx
+++ app/components/ContactFormy.tsx
@@ -62,7 +62,7 @@ const ContactForm: React.FC = () => {
       <Container>
         <ToastContainer />
         <FadeIn>
-          <form onSubmit={onSubmit} className="max-w-4xl md:w-[540px] w-full">
+          <form onSubmit={onSubmit} className="md:w-[540px] w-full">
             <h2 className="font-display text-base font-semibold text-sky-900 uppercase mt-2">
               Work Inquiries
             </h2>
